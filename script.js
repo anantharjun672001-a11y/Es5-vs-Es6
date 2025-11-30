b9ef83c23d0d8c161cbea8653cb7914f6075e5c1
anantharjun672001-a11y <anantharjun672001@gmail.com>
2025-11-30 09:14:48 +0530

    block level scope let

---
+++ script.js
@@ -0,0 +1,16 @@
+// Scoping : var vs let vs const 
+//Var example :- function / global scope 
+
+/* if(true){
+    var x=10;
+}
+console.log(x);
+ */
+
+//let example :- block scope 
+
+if(true){
+    let x=10;
+    console.log(x);
+}
+
